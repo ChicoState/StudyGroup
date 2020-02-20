1f84806f026fa9ec50a528536e75f3fd15b4df0d
bmarden <36089218+bmarden@users.noreply.github.com>
2020-02-19 19:54:00 -0800

    Update main.dart, leave comments in for clarity

--- study_group_app/lib/main.dart
+++ study_group_app/lib/main.dart
@@ -9,13 +9,31 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
         primarySwatch: Colors.blue,
       ),
+     
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
@@ -24,13 +42,29 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // Right here they had an additional function that increased a counter. 
+  // They are just talking about how to update the view when something changes in the app
+  // by calling setState() Below is original comment.
+  
+  // setState tells the Flutter framework that something has
+  // changed in this State, which causes it to rerun the build method below
+  // so that the display can reflect the updated values.
   @override
   Widget build(BuildContext context) {
+     // The build method is rerun every time setState is called
+    
     return Scaffold(
       appBar: AppBar(
+        
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(),
+      body: Center(
+      // Center is a layout widget. It takes a single child and positions it
+     // in the middle of the parent.
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+       
     );
   }
 }
