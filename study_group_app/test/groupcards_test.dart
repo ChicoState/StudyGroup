6203d5aac24b7c48b0b5735e6576dec27f0b4211
Asheela Magwili <asheelamagwili@wlan-t-201-211-dhcp.CSUChico.EDU>
2020-04-23 14:11:41 -0700

    Testing groupcards

---
+++ study_group_app/test/groupcards_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:study_group_app/screens/groups/group_card.dart';
+
+void main() {
+  group( 'Creating new group study session.', () {
+
+    // Tests group card
+    test('New group study session appear as card on home page', (){
+      TimeOfDay _startTime;
+      TimeOfDay _endTime;
+      String groupName;
+      String day;
+      int maxMembers;
+      String location;
+    });
+  });
+}
